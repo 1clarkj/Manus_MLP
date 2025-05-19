f77417b4b7bc5bdff660ee2632efbe3896117441
1clarkj <162683919+1clarkj@users.noreply.github.com>
2025-05-19 19:20:45 +0200

    Update deploy_pi.py

--- deploy_pi.py
+++ deploy_pi.py
@@ -7,3 +7,25 @@ from tkinter import ttk
 import threading
 import warnings
 warnings.filterwarnings("ignore", message="X does not have valid feature names.*")
+
+
+# UDP server setup
+UDP_IP = "192.168.0.115"
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+print(f"Listening on {UDP_IP}:{UDP_PORT}")  
+
+while True:
+    try:
+        data, addr = sock.recvfrom(1024)
+        input_data = np.frombuffer(data, dtype=np.float32).reshape(1, -1)
+
+        # Apply the same normalization
+        input_data_scaled = scaler.transform(input_data)
+
+        # Predict and print softmax probabilities
+        probs = model.predict_proba(input_data_scaled)
+        print(f"Softmax Probabilities: {probs}")
+    except Exception as e:
+        print(f"Error: {e}")
