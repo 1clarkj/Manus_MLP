316c5389df19217ec9e535c9052a0e8a8d65167b
1clarkj <162683919+1clarkj@users.noreply.github.com>
2025-05-19 19:18:01 +0200

    Update deploy_pi.py

--- deploy_pi.py
+++ deploy_pi.py
@@ -1,1 +1,9 @@
-
+import socket
+import joblib
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import tkinter as tk
+from tkinter import ttk
+import threading
+import warnings
+warnings.filterwarnings("ignore", message="X does not have valid feature names.*")
